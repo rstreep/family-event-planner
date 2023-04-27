23c52b5201da9d81dbc003f983a6ad6b241b5e19
Alice <aliceli083@gmail.com>
2023-04-26 20:10:23 -0700

    added zoom and formblock

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// var map = L.map('map').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+
+// setting parameters
+var map= L.map('map', {
+    layer: MQ.mapLayer(),
+    center: [49.228537499999995, -123.10585798903601],
+    zoom: 12
+});
