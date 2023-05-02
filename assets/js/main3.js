9f1f14d315b98248c51b18e59ce1b6469e72348a
mayamouse <cooperpontea@gmail.com>
2023-05-01 21:01:46 -0400

    added previous button functionality

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -14,12 +14,13 @@ nextButton.addEventListener('click', (event) => {
     currentStep += 1
   })
 
-
   previousButton.addEventListener('click', (event) => {
     event.preventDefault()
     tabPanels[currentStep].classList.add('hidden')
     tabTargets[currentStep].classList.remove('active')
     tabPanels[currentStep - 1].classList.remove('hidden')
     tabTargets[currentStep - 1].classList.add('active')
-    currentStep += 1
+    currentStep -= 1
   })
+
+  
