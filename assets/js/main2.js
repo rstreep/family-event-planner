87dab94b1a5316f43d0e9f0834fbc722a44a3052
rstreep <127055958+rstreep@users.noreply.github.com>
2023-05-02 01:53:46 -0400

    added function to grab menu items from edamam API

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -13,3 +13,64 @@ nextButton.addEventListener('click', (event) => {
     tabTargets[currentStep + 1].classList.add('active')
     currentStep += 1
   })
+
+
+// Get Menu API
+
+var apiID = "e0e48aa8";
+var apiKey = "5ecc0a6a74140b8afe687fc73be0ddb2";
+var genBtn = document.querySelector('#generate');
+
+genBtn.onclick = function () {
+    var userSearch = document.querySelector('#userSearch').value;
+    console.log(userSearch);
+    event.preventDefault();
+    var dietEl = document.querySelector('#diets').value;
+    var healthEl = document.querySelector('#health-concerns').value;
+    var menu = '';
+
+    fetch(`https://api.edamam.com/api/recipes/v2?type=any&dishType=Starter&app_id=${apiID}&app_key=${apiKey}&diet=${dietEl}&health=${healthEl}&random=true`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var menu = data.hits.map( food => {
+                return `<a href="${food.recipe.url}">${food.recipe.label}</a>`; 
+            }
+            );
+            console.log(menu[0]);
+            console.log(menu[1]);
+            document.querySelector('#app1').insertAdjacentHTML("afterbegin", menu[0]);
+            document.querySelector('#app2').insertAdjacentHTML("afterbegin", menu[1]);
+        })
+
+    fetch(`https://api.edamam.com/api/recipes/v2?q=${userSearch}&type=any&dishType=Main%20course&app_id=${apiID}&app_key=${apiKey}&diet=${dietEl}&health=${healthEl}&random=true`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var menu = data.hits.map( food => {
+                return `<a href="${food.recipe.url}">${food.recipe.label}</a>`; 
+            }
+            );
+            console.log(menu[0]);
+            console.log(menu[1]);
+            document.querySelector('#ent1').insertAdjacentHTML("afterbegin", menu[0]);
+            document.querySelector('#ent2').insertAdjacentHTML("afterbegin", menu[1]);
+        })
+
+        fetch(`https://api.edamam.com/api/recipes/v2?type=any&dishType=Desserts&app_id=${apiID}&app_key=${apiKey}&diet=${dietEl}&health=${healthEl}&random=true`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var menu = data.hits.map( food => {
+                return `<a href="${food.recipe.url}">${food.recipe.label}</a>`; 
+            }
+            );
+            console.log(menu[0]);
+            console.log(menu[1]);
+            document.querySelector('#des1').insertAdjacentHTML("afterbegin", menu[0]);
+            document.querySelector('#des2').insertAdjacentHTML("afterbegin", menu[1]);
+        })
+}
