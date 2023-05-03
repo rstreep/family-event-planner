a5983b02cf99415b7f0c9bb9b82aab7d980d6bd2
rstreep <127055958+rstreep@users.noreply.github.com>
2023-05-02 22:33:20 -0400

    finalized map api integration

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,27 +1,27 @@
-const previousButton = document.querySelector('#prev')
-const nextButton = document.querySelector('#next')
-const submitButton = document.querySelector('#submit')
-const tabTargets = document.querySelectorAll('.tab')
-const tabPanels = document.querySelectorAll('.tabpanel')
-let currentStep = 0
+// const previousButton = document.querySelector('#prev')
+// const nextButton = document.querySelector('#next')
+// const submitButton = document.querySelector('#submit')
+// const tabTargets = document.querySelectorAll('.tab')
+// const tabPanels = document.querySelectorAll('.tabpanel')
+// let currentStep = 0
 
-nextButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    tabPanels[currentStep].classList.add('hidden')
-    tabTargets[currentStep].classList.remove('active')
-    tabPanels[currentStep + 1].classList.remove('hidden')
-    tabTargets[currentStep + 1].classList.add('active')
-    currentStep += 1
-})
+// nextButton.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     tabPanels[currentStep].classList.add('hidden')
+//     tabTargets[currentStep].classList.remove('active')
+//     tabPanels[currentStep + 1].classList.remove('hidden')
+//     tabTargets[currentStep + 1].classList.add('active')
+//     currentStep += 1
+// })
 
-previousButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    tabPanels[currentStep].classList.add('hidden')
-    tabTargets[currentStep].classList.remove('active')
-    tabPanels[currentStep - 1].classList.remove('hidden')
-    tabTargets[currentStep - 1].classList.add('active')
-    currentStep -= 1
-  })
+// previousButton.addEventListener('click', (event) => {
+//     event.preventDefault()
+//     tabPanels[currentStep].classList.add('hidden')
+//     tabTargets[currentStep].classList.remove('active')
+//     tabPanels[currentStep - 1].classList.remove('hidden')
+//     tabTargets[currentStep - 1].classList.add('active')
+//     currentStep -= 1
+//   })
 
 // Get Menu API
 
