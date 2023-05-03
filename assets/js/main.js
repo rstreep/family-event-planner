034860a69027beba5acf32d7a9b518d47eda9fbb
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-05-02 22:54:15 -0400

    did updates to the data structure to reflect menu page structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,3 @@
-const previousButton = document.querySelector('#prev')
-const nextButton = document.querySelector('#next')
-const submitButton = document.querySelector('#submit')
-const tabTargets = document.querySelectorAll('.tab')
-const tabPanels = document.querySelectorAll('.tabpanel')
-let currentStep = 0
-
-nextButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    tabPanels[currentStep].classList.add('hidden')
-    tabTargets[currentStep].classList.remove('active')
-    tabPanels[currentStep + 1].classList.remove('hidden')
-    tabTargets[currentStep + 1].classList.add('active')
-    currentStep += 1
-})
-
-previousButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    tabPanels[currentStep].classList.add('hidden')
-    tabTargets[currentStep].classList.remove('active')
-    tabPanels[currentStep - 1].classList.remove('hidden')
-    tabTargets[currentStep - 1].classList.add('active')
-    currentStep -= 1
-  })
-
-
 
 // /**
 //  * the section below describes the global Variables
@@ -51,75 +25,62 @@ eventObj = {
     eventName: "Family Reunion",
     eventDate: "Thu May 4th, 2023, 7pm est",
     eventLocation: "The Franklin Institute, Center City, Address: 222 N 20th Street, Philadelphia, PA 19103",
-    weather: {},
-    isOutside: false,
-    diet: ['vegan', 'peanut-free', 'spicy-hot'],
+    dietReswtrictions: 'vegan',
+    healthConcerns: 'peanut-free',
     menuItems: [
         {
             dishType: "Appetizer 1",
             dishName: "shrimp cocktail",
-            coocingtime: "15 min",
-            ingridients: ['6 cups water', '1 small lemon thinly sliced', '1 teaspoon minced fresh garlic', '2 bay leaves', '5 black peppercorns', '3 to 4 sprigs fresh Italian parsley', '¾ teaspoon kosher salt', '1 pound medium to large shrimp peeled and deveined (tails left on)'],
-            recipe: "Bring water to a boil. Add shrimp, cover; remove from heat. Let stand until shrimp are  bright pink and curled; 3 to 5 minutes. Drain and transfer to prepared tray of ice to cool completely.",
-            imageLink: "/Users/innafedorenko/family-event-planner/assets/images/shrimpt_coctail.jpeg",
+            dishLink: 'http://127.0.0.1:5500/index.html?diets=low-carb&health-concerns=vegetarian#:~:text=Appetizer-,Oven%20Scrambled%20Eggs,-Smokey%20Deviled%20Eggs',
+
         }, {
             dishType: "Appetizer 2",
             dishName: "shrimp cocktail",
-            coocingtime: "15 min",
-            ingridients: ['6 cups water', '1 small lemon thinly sliced', '1 teaspoon minced fresh garlic', '2 bay leaves', '5 black peppercorns', '3 to 4 sprigs fresh Italian parsley', '¾ teaspoon kosher salt', '1 pound medium to large shrimp peeled and deveined (tails left on)'],
-            recipe: "Bring water to a boil. Add shrimp, cover; remove from heat. Let stand until shrimp are  bright pink and curled; 3 to 5 minutes. Drain and transfer to prepared tray of ice to cool completely.",
-            imageLink: "/Users/innafedorenko/family-event-planner/assets/images/shrimpt_coctail.jpeg",
+            dishLink: 'http://127.0.0.1:5500/index.html?diets=low-carb&health-concerns=vegetarian#:~:text=Appetizer-,Oven%20Scrambled%20Eggs,-Smokey%20Deviled%20Eggs',
         }, {
             dishType: "Main Dish",
             dishName: "shrimp cocktail",
-            coocingtime: "15 min",
-            ingridients: ['6 cups water', '1 small lemon thinly sliced', '1 teaspoon minced fresh garlic', '2 bay leaves', '5 black peppercorns', '3 to 4 sprigs fresh Italian parsley', '¾ teaspoon kosher salt', '1 pound medium to large shrimp peeled and deveined (tails left on)'],
-            recipe: "Bring water to a boil. Add shrimp, cover; remove from heat. Let stand until shrimp are  bright pink and curled; 3 to 5 minutes. Drain and transfer to prepared tray of ice to cool completely.",
-            imageLink: "/Users/innafedorenko/family-event-planner/assets/images/shrimpt_coctail.jpeg",
+            dishLink: 'http://127.0.0.1:5500/index.html?diets=low-carb&health-concerns=vegetarian#:~:text=Appetizer-,Oven%20Scrambled%20Eggs,-Smokey%20Deviled%20Eggs',
         }, {
             dishType: "Side 1",
             dishName: "shrimp cocktail",
-            coocingtime: "15 min",
-            ingridients: ['6 cups water', '1 small lemon thinly sliced', '1 teaspoon minced fresh garlic', '2 bay leaves', '5 black peppercorns', '3 to 4 sprigs fresh Italian parsley', '¾ teaspoon kosher salt', '1 pound medium to large shrimp peeled and deveined (tails left on)'],
-            recipe: "Bring water to a boil. Add shrimp, cover; remove from heat. Let stand until shrimp are  bright pink and curled; 3 to 5 minutes. Drain and transfer to prepared tray of ice to cool completely.",
-            imageLink: "/Users/innafedorenko/family-event-planner/assets/images/shrimpt_coctail.jpeg",
+            dishLink: 'http://127.0.0.1:5500/index.html?diets=low-carb&health-concerns=vegetarian#:~:text=Appetizer-,Oven%20Scrambled%20Eggs,-Smokey%20Deviled%20Eggs',
         }, {
             dishType: "Side 2",
             dishName: "shrimp cocktail",
-            coocingtime: "15 min",
-            ingridients: ['6 cups water', '1 small lemon thinly sliced', '1 teaspoon minced fresh garlic', '2 bay leaves', '5 black peppercorns', '3 to 4 sprigs fresh Italian parsley', '¾ teaspoon kosher salt', '1 pound medium to large shrimp peeled and deveined (tails left on)'],
-            recipe: "Bring water to a boil. Add shrimp, cover; remove from heat. Let stand until shrimp are  bright pink and curled; 3 to 5 minutes. Drain and transfer to prepared tray of ice to cool completely.",
-            imageLink: "/Users/innafedorenko/family-event-planner/assets/images/shrimpt_coctail.jpeg",
+            dishLink: 'http://127.0.0.1:5500/index.html?diets=low-carb&health-concerns=vegetarian#:~:text=Appetizer-,Oven%20Scrambled%20Eggs,-Smokey%20Deviled%20Eggs',
         }, {
             dishType: "Dessert",
             dishName: "shrimp cocktail",
-            coocingtime: "15 min",
-            ingridients: ['6 cups water', '1 small lemon thinly sliced', '1 teaspoon minced fresh garlic', '2 bay leaves', '5 black peppercorns', '3 to 4 sprigs fresh Italian parsley', '¾ teaspoon kosher salt', '1 pound medium to large shrimp peeled and deveined (tails left on)'],
-            recipe: "Bring water to a boil. Add shrimp, cover; remove from heat. Let stand until shrimp are  bright pink and curled; 3 to 5 minutes. Drain and transfer to prepared tray of ice to cool completely.",
-            imageLink: "/Users/innafedorenko/family-event-planner/assets/images/shrimpt_coctail.jpeg",
-        }],
-    drinks: [
-        {
-            typeDrink: "water",
-            nameDrink: "sparkling",
-            kidsAllowed: "yes"
-        }, {
-            typeDrink: "vine",
-            nameDrink: "Malbec",
-            kidsAllowed: "no"
-        }, {
-            typeDrink: "cola",
-            nameDrink: "diet cola",
-            kidsAllowed: "yes"
-        }, {
-            typeDrink: "tea",
-            nameDrink: "green",
-            kidsAllowed: "yes"
-        }],
-    advertisements: ['cards games', 'dancing', 'live music']
+            dishLink: 'http://127.0.0.1:5500/index.html?diets=low-carb&health-concerns=vegetarian#:~:text=Appetizer-,Oven%20Scrambled%20Eggs,-Smokey%20Deviled%20Eggs',
+        }]
 }
 
 var directionsButton = document.querySelector('#getDirections');
+const previousButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+const submitButton = document.querySelector('#submit')
+const tabTargets = document.querySelectorAll('.tab')
+const tabPanels = document.querySelectorAll('.tabpanel')
+let currentStep = 0
+
+nextButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    tabPanels[currentStep].classList.add('hidden')
+    tabTargets[currentStep].classList.remove('active')
+    tabPanels[currentStep + 1].classList.remove('hidden')
+    tabTargets[currentStep + 1].classList.add('active')
+    currentStep += 1
+})
+
+previousButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    tabPanels[currentStep].classList.add('hidden')
+    tabTargets[currentStep].classList.remove('active')
+    tabPanels[currentStep - 1].classList.remove('hidden')
+    tabTargets[currentStep - 1].classList.add('active')
+    currentStep -= 1
+  })
 
 directionsButton.addEventListener('click', (event) => {
     console.log('working');
@@ -181,89 +142,45 @@ function getEventData() {
 
 window.addEventListener("load", init);
 
+// /**
+//  * This function send invites to the guests by clicking Sans button
+//  */
+// function sendClicked() {
+//     // Define what should happen when the Send button is clicked
+//     console.log('Send button clicked! 0 ');
+//     // ////////////////// POC of mailTo    /////////////////////////////////////////////
+//     // Define the event object with details
+//     var event = {
+//         name: "Family Reunion",
+//         location: "123 Main Street",
+//         menu: "BBQ, hamburgers, hotdogs, and salads"
+//     };
+//     // Create an empty array to hold the email addresses
+//     var emailList = [];
 
-/**
- * This function navigates to the main page to continue edining by selecting Back button
- * screen should be populated with user unput get from local storage
- */
-function backClicked() {
-    // Define what should happen when the Back button is clicked
-    console.log('Back button clicked!');
-    //ToDo  - navigate to the previous page
-    //ToDo - get values from local storage
-    //ToDo - render html elements
-}
-/**
- * This funtion save event details to Local Storage by clicking Save button
- */
-function saveClicked() {
-    // Define what should happen when the Save button is clicked
-    console.log('Save button clicked!');
-    //ToDo  - save final objects to local storage
-    //ToDo - navigate to the firsr page
-    //ToDo  - get data from local storage
-    //ToDo - render html elements
+//     // Loop through the guestsList array and retrieve the email addresses
+//     for (var i = 0; i < guestsList.length; i++) {
+//         emailList.push(guestsList[i].email);
+//     }
 
-}
+//     // Create the email subject
+//     var subject = "Invitation to " + event.name;
 
-/**
- * This function send invites to the guests by clicking Sans button
- */
-function sendClicked() {
-    // Define what should happen when the Send button is clicked
-    console.log('Send button clicked! 0 ');
-    // ////////////////// POC of mailTo    /////////////////////////////////////////////
-    // Define the event object with details
-    var event = {
-        name: "Family Reunion",
-        location: "123 Main Street",
-        menu: "BBQ, hamburgers, hotdogs, and salads"
-    };
-    // Create an empty array to hold the email addresses
-    var emailList = [];
+//     // Create the email body with the event details
+//     var body = "Dear family and friends,\n\nPlease join us for the " + event.name + " at " + event.location + ".\n\nThe menu includes " + event.menu + ".\n\nWe hope to see you there!\n\nBest regards,\nYour Name";
 
-    // Loop through the guestsList array and retrieve the email addresses
-    for (var i = 0; i < guestsList.length; i++) {
-        emailList.push(guestsList[i].email);
-    }
+//     // Create the mailto URL with the email addresses, subject, and body
+//     var mailtoUrl = "mailto:" + emailList.join(",") + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
 
-    // Create the email subject
-    var subject = "Invitation to " + event.name;
-
-    // Create the email body with the event details
-    var body = "Dear family and friends,\n\nPlease join us for the " + event.name + " at " + event.location + ".\n\nThe menu includes " + event.menu + ".\n\nWe hope to see you there!\n\nBest regards,\nYour Name";
-
-    // Create the mailto URL with the email addresses, subject, and body
-    var mailtoUrl = "mailto:" + emailList.join(",") + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
-
-    // Open the mailto dialog box
-    window.location.href = mailtoUrl;
+//     // Open the mailto dialog box
+//     window.location.href = mailtoUrl;
 
 
-    /////////////////////////////////////////////////////////////////
-    //ToDo  - save final objects to local storage
-    //ToDo - open MailTo dialog with pre-populated data 
-    //ToDo - navigate to the firsr page
-    //ToDo  - get data from local storage
-    //ToDo - render html elements
-}
+//     /////////////////////////////////////////////////////////////////
+//     //ToDo  - save final objects to local storage
+//     //ToDo - open MailTo dialog with pre-populated data 
+//     //ToDo - navigate to the firsr page
+//     //ToDo  - get data from local storage
+//     //ToDo - render html elements
+// }
 
-
-/**
- * This is main function that is going to run during opening the preview page
- */
-//console.log ('Script is connected');
-
-
-    // Define click event handlers for each button
-    $('#backBtn').click(function () {
-        backClicked();
-    });
-
-    $('#saveBtn').click(function () {
-        saveClicked();
-    });
-
-    $('#sendBtn').click(function () {
-        sendClicked();
-    });
